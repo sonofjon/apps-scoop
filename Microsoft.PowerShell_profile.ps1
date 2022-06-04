d670d63c246906bee51a9e6dfdff52149082ffba
Andreas Jonsson <ajdev8@gmail.com>
2022-06-04 23:04:33 +0200

    Add PowerShell profile for scoop completion

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+# PowerShell profile
+
+# scoop completion
+# Import-Module C:\Users\Andreas Jonsson\scoop\modules\scoop-completion
+Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
